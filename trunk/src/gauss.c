dc566ac789ed5460f23b526612c48dc1b66505ae
tulipanik3122000 <01169558@pw.edu.pl>
2021-12-20 13:56:51 +0100

    Poprawiona implementacja gaussa, bez segmentation fault

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -7,21 +7,20 @@
  */
 int eliminate(Matrix *mat, Matrix *b){
 	int i,j,k;
-	int g=0;
-	printf("kontrol0");
+	double dziel;
 	for(i=0; i<mat->c; i++){
-		if((mat->data[i][g])==0)
+		if((mat->data[i][i])==0)
 			return 1;
-		for(j=i+1; j<mat->c; j++){
-			for(k=g; k<mat->r;k++)
-				mat->data[j][k]=(mat->data[j][k])/(mat->data[i][k]);
-			b->data[j][0]==(b->data[j][0])/(b->data[i][0]);
-			printf("kontrol");
-		}
-		g++;
-		printf("kontrol2");
+		for(j=i+1; j<mat->r; j++){
+			dziel=((mat->data[j][i])/(mat->data[i][i]));
+			printf("%g",dziel);
+			for(k=i; k<mat->c;k++)
+				mat->data[j][k]=(mat->data[j][k])-((mat->data[i][k])*dziel);
+
+			b->data[j][0]=(b->data[j][0])-((b->data[i][0])*dziel);
+		
 	}
-	printf("kontrol3");
+	}
 
 		return 0;
 }
